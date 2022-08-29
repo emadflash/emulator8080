b4986b5dc53b5f2727e2af75f170c8a23196966f
madflash <backbugkaught@gmail.com>
2022-08-29 19:43:18 +0530

    delete Makefile

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include "emulator.c"
 
+static void cli_help_message() { eprintln("Usage: ./emulator8080 ROM"); }
+
 int main(int argc, char **argv) {
     if (argc < 2) {
-        eprintln("Error: expected [ROM]");
+        cli_help_message();
         exit(1);
     }
 
     String rom = file_as_string(argv[1]);
-    Cpu *cpu = make_cpu(cast(u8*) rom, string_length(rom), 0x0000);
+    Cpu *cpu = make_cpu(cast(u8 *) rom, string_length(rom), 0x0000);
 
     cpu_execute(cpu);
 
